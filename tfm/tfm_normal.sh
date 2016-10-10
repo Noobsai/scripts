0054f4d4b894fb875596b52ca1b0361ed50ac462
Noobsai <Noobsai@users.noreply.github.com>
2016-10-10 22:49:32 +0500

    added tfm helper

---
+++ tfm/tfm_normal.sh
@@ -0,0 +1,3 @@
+gsettings set org.gnome.desktop.peripherals.keyboard delay 500
+gsettings set org.gnome.desktop.peripherals.keyboard repeat true
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 30
---
+++ tfm/tfm_ultra.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+gsettings set org.gnome.desktop.peripherals.keyboard delay 1
+gsettings set org.gnome.desktop.peripherals.keyboard repeat true
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 1

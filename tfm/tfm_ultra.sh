0972d4fd9068f83272f96c8feb98c00ba9774e9d
Noobsai <Noobsai@users.noreply.github.com>
2016-10-10 22:57:13 +0500

    removed bash header

--- tfm/tfm_ultra.sh
+++ tfm/tfm_ultra.sh
@@ -1,4 +1,3 @@
-#!/bin/bash 
 gsettings set org.gnome.desktop.peripherals.keyboard delay 1
 gsettings set org.gnome.desktop.peripherals.keyboard repeat true
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 1

13754ed9369bed40a67c99bd233daf0d1537fef8
Noobsai <Noobsai@users.noreply.github.com>
2017-02-16 00:05:13 +0500

    added executing lock

--- tfm/tfm_normal.sh
+++ tfm/tfm_normal.sh
@@ -1,3 +1,11 @@
-gsettings set org.gnome.desktop.peripherals.keyboard delay 500
-gsettings set org.gnome.desktop.peripherals.keyboard repeat true
-gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 30
+#!/bin/bash
+TFM_ULTRA=/tmp/TFM_ULTRA.lock
+TFM_NORMAL=/tmp/TFM_NORMAL.lock
+
+if mkdir $TFM_NORMAL
+then
+	rmdir $TFM_ULTRA
+	gsettings set org.gnome.desktop.peripherals.keyboard delay 100
+	gsettings set org.gnome.desktop.peripherals.keyboard repeat true
+	gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 9
+fi
--- tfm/tfm_ultra.sh
+++ tfm/tfm_ultra.sh
@@ -1,3 +1,11 @@
-gsettings set org.gnome.desktop.peripherals.keyboard delay 1
-gsettings set org.gnome.desktop.peripherals.keyboard repeat true
-gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 1
+#!/bin/bash
+TFM_ULTRA=/tmp/TFM_ULTRA.lock
+TFM_NORMAL=/tmp/TFM_NORMAL.lock
+
+if mkdir $TFM_ULTRA
+then
+	rmdir $TFM_NORMAL
+	gsettings set org.gnome.desktop.peripherals.keyboard delay 1
+	gsettings set org.gnome.desktop.peripherals.keyboard repeat true
+	gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 1
+fi

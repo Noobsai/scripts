a56e0dd939b315c3623dc31020810ef68a4c1088
Noobsai <ewgdogg@gmail.com>
2022-07-10 18:46:49 +0500

    Using mounting

--- btrfs.sh
+++ btrfs.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+mkdir /mnt/root
+mount -U 865be7a7-8805-4dd8-962f-a4ad4624ac74 /mnt/root
+btrfs subvolume snapshot -r /mnt/root/@ /mnt/root/timeshift-btrfs/snapshots
 btrfs subvolume delete /data/snapshots/@
-btrfs subvolume snapshot -r /run/timeshift/backup/@ /run/timeshift/backup/timeshift-btrfs/snapshots
-btrfs send /run/timeshift/backup/timeshift-btrfs/snapshots/@ | btrfs receive /data/snapshots
-btrfs subvolume delete /run/timeshift/backup/timeshift-btrfs/snapshots/@
+btrfs send /mnt/root/timeshift-btrfs/snapshots/@ | btrfs receive /data/snapshots
+btrfs subvolume delete /mnt/root/timeshift-btrfs/snapshots/@
+umount /mnt/root
+rmdir /mnt/root
 
+mkdir /mnt/home
+mount -U e65d65bf-6509-42bf-9dd1-a749a603a572 /mnt/home
+btrfs subvolume snapshot -r /mnt/home/@home /mnt/home/timeshift-btrfs/snapshots
 btrfs subvolume delete /data/snapshots/@home
-btrfs subvolume snapshot -r /run/timeshift/backup-home/@home /run/timeshift/backup-home/timeshift-btrfs/snapshots
-btrfs send /run/timeshift/backup-home/timeshift-btrfs/snapshots/@home | btrfs receive /data/snapshots
-btrfs subvolume delete /run/timeshift/backup-home/timeshift-btrfs/snapshots/@home
+btrfs send /mnt/home/timeshift-btrfs/snapshots/@home | btrfs receive /data/snapshots
+btrfs subvolume delete /mnt/home/timeshift-btrfs/snapshots/@home
+umount /mnt/home
+rmdir /mnt/home
